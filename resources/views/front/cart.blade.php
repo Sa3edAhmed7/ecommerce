40920839084eb3599cc8c74df4a1c81b4a381ec7
Sa3edAhmed7 <saeidahmed774@gmail.com>
2022-10-23 15:54:47 +0200

    Third ecommerce demo project

--- resources/views/front/cart.blade.php
+++ resources/views/front/cart.blade.php
@@ -60,12 +60,14 @@
         </button>
     </div>				
 @endif
-                @php
+               
+				<div class="wrap-iten-in-cart">
+				@php
 				$cart_array = cardArray()
 				@endphp
-				<div class="wrap-iten-in-cart">
 				@if( count($cart_array)  > 0)
 					<h3 class="box-title">Products Name</h3>
+					<h4 style="color:#ff2832;">To Increase The Quantity Of The Product, Click On The Name or Image of The Product</h4>
 					@foreach ($cart_array as $add_cart )
 					@php
 					$images = $add_cart['attributes'][0];
@@ -73,19 +75,17 @@
 					$images = $images[0];
 					@endphp
 					<ul class="products-cart">
-						<li class="pr-cart-item">
+						<li class="pr-cart-item">			
 							<div class="product-image">
-								<figure><img src="{{asset($images)}}" alt="" width="100px" height="100px"></figure>
+								<a href="/productdetail/{{ $add_cart['id'] }}"><img src="{{asset($images)}}" alt="" width="100px" height="100px"></a>
 							</div>
 							<div class="product-name">
-								<a class="link-to-product" href="#">{{ $add_cart['name'] }}</a>
+								<a class="link-to-product" href="/productdetail/{{ $add_cart['id'] }}">{{ $add_cart['name'] }}</a>
+								<div class="product-name">
+										<h4>{{ $add_cart['quantity'] }}</h4>
+								</div>	
 							</div>
-							<div class="price-field produtc-price"><p class="price">${{ $add_cart['price'] }}</p></div>							
-									<div class="input-number" style="width: 20%; margin-top: 30px;">
-										<input type="number" name="quantity" value="{{ $add_cart['quantity'] }}">
-										<span class="qty-up">+</span>
-										<span class="qty-down">-</span>
-									</div>
+							<div class="price-field produtc-price"><p class="price">${{ $add_cart['price'] }}</p></div>					
 							<div class="delete">
 								<a href="{{ url('/delete-cart/'.$add_cart['id']) }}" class="btn btn-delete" title="">
 									<span>Delete from your cart</span>
@@ -95,21 +95,20 @@
 						</li>			
 					</ul>
 					@endforeach
-					@else
-                    <p>No item in cart</p>
-                @endif
 				</div>
 
 				<div class="summary">
 					<div class="order-summary">
 						<h4 class="title-box">Order Summary</h4>
-						<p class="summary-info"><span class="title">Subtotal</span><b class="index">${{Cart::getTotal()}}</b></p>
 						<p class="summary-info"><span class="title">Shipping</span><b class="index">Free Shipping</b></p>
-						<p class="summary-info total-info "><span class="title">Total</span><b class="index">${{Cart::getTotal()}}</b></p>
+						<h3 class="summary-info total-info "><span class="title">Total :</span><b class="index" style="color:#ff2832;"> ${{Cart::getTotal()}}</b></h3>
 					</div>
 						<a class="btn btn-checkout" style="font-weight: bold;color: #D10024;text-align: right;" href="{{route('checkout.index')}}">Check out</a>
 						<a class="btn btn-checkout" style="font-weight: bold;color: #D10024;text-align: right;" href="{{ route('allproducts.index')}}">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
 				</div>
+				@else
+                    <p>No item in cart</p>
+                @endif
             </div>
 		</div>
     @endsection
